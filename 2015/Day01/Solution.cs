df33cb38a36689586a124da18994c584d59a8a0e
Vinay Kapadia <dev@vkapadia.com>
2022-12-02 18:47:34 +0000

    Bug Fix

---
+++ 2015/Day01/Solution.cs
@@ -0,0 +1,16 @@
+namespace AdventOfCode._2015.Day01;
+
+[ProblemName("")]      
+internal class Solution : ISolver
+{
+
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
