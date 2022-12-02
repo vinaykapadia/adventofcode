2033d96cced32961dbf8bd42b52fdbd63085d41b
Vinay Kapadia <dev@vkapadia.com>
2022-12-02 18:41:48 +0000

    Updates

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -2,7 +2,4 @@
 global using System.Collections.Generic;
 global using System.Linq;
 
-global using AdventOfCode.Common;
 global using AdventOfCode.Lib;
-global using static AdventOfCode.Common.Utilities;
-global using static AdventOfCode._2019.IntcodeComputer;
