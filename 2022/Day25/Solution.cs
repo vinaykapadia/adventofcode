6f3d80dc7ea872b354ac4ba809997cb3790ea781
Vinay Kapadia <dev@vkapadia.com>
2023-11-30 23:15:14 +0000

    2022

---
+++ 2022/Day25/Solution.cs
@@ -0,0 +1,16 @@
+namespace AdventOfCode._2022.Day25;
+
+[ProblemName("Full of Hot Air")]      
+internal class Solution : ISolver
+{
+
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
