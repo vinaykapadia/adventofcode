b1cccb4e15075e1fd4846023c1addf0adbcd88a9
Vinay Kapadia <dev@vkapadia.com>
2023-11-30 23:15:14 +0000

    2022

---
+++ 2022/Day23/Solution.cs
@@ -0,0 +1,16 @@
+namespace AdventOfCode._2022.Day23;
+
+[ProblemName("Unstable Diffusion")]      
+internal class Solution : ISolver
+{
+
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
---
+++ 2022/Day24/Solution.cs
@@ -0,0 +1,16 @@
+namespace AdventOfCode._2022.Day24;
+
+[ProblemName("Blizzard Basin")]      
+internal class Solution : ISolver
+{
+
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
