7dd4f82798830ac79a5bd9c1736f1550680298b3
Vinay Kapadia <dev@vkapadia.com>
2023-11-30 22:38:54 +0000

    2021

---
+++ 2021/Day25/Solution.cs
@@ -0,0 +1,16 @@
+namespace AdventOfCode._2021.Day25;
+
+[ProblemName("Sea Cucumber")]      
+internal class Solution : ISolver
+{
+
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
