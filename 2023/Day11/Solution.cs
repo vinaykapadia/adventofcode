e7c6d74d1d06d33f8976fa1b183661ab9351e241
Vinay Kapadia <dev@vkapadia.com>
2024-01-22 22:14:27 +0000

    2023

---
+++ 2023/Day11/Solution.cs
@@ -0,0 +1,16 @@
+namespace AdventOfCode._2023.Day11;
+
+[ProblemName("Cosmic Expansion")]      
+internal class Solution : ISolver
+{
+
+    public object PartOne(string input)
+    {
+        return 0;
+    }
+
+    public object PartTwo(string input)
+    {
+        return 0;
+    }
+}
