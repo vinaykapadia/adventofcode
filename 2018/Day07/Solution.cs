3a6db67bc8c1245958a5aacb6a021d682a085ff6
Vinay Kapadia <dev@vkapadia.com>
2024-11-13 17:30:20 +0000

    Fixes.

--- 2018/Day07/Solution.cs
+++ 2018/Day07/Solution.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
-using AdventOfCode.Lib;
 
 namespace AdventOfCode._2018.Day07;
 
---
+++ Common/AutoAddingDictionary.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode.Common;
+
+public class AutoAddingDictionary<TKey, TValue> : Dictionary<TKey, TValue> where TValue : new()
+{
+	public new TValue this[TKey key]
+	{
+		get
+		{
+			if (!ContainsKey(key))
+				Add(key, new TValue());
+			return base[key];
+		}
+		set
+		{
+			if (!ContainsKey(key))
+				Add(key, value);
+			base[key] = value;
+		}
+	}
+}
