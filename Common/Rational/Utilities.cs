8182d7103d48edb07c62c56eabf7a5c846a37815
Vinay Kapadia <dev@vkapadia.com>
2025-07-28 20:57:50 +0100

    Adding utilities.

---
+++ Common/Rational/Utilities.cs
@@ -0,0 +1,32 @@
+﻿namespace AdventOfCode.Common.Rational;
+
+public partial struct Rational
+{
+    public static Rational FromDouble(double value, int maxIterations = int.MaxValue, double threshold = double.Epsilon)
+    {
+        var whole = (long)Math.Floor(value);
+        value -= whole;
+
+        if (value < threshold)
+            return new Rational(whole);
+
+        if (1 - threshold < value)
+            return new Rational(whole + 1);
+
+        var low = new Rational(0);
+        var high = new Rational(1);
+
+        for (var i = 0; i < maxIterations; ++i)
+        {
+            var mid = new Rational(low.Numerator + high.Numerator, low.Denominator + high.Denominator);
+            if (mid.Numerator > mid.Denominator * (value + threshold))
+                high = mid;
+            else if (mid.Numerator < mid.Denominator * (value - threshold))
+                low = mid;
+            else
+                return new Rational(whole * mid.Denominator + mid.Numerator, mid.Denominator);
+        }
+
+        throw new ArithmeticException("Failed to solve.");
+    }
+}
---
+++ Common/Utilities.cs
@@ -0,0 +1,42 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace AdventOfCode.Common;
+
+public static class Utilities
+{
+    public static object PartBAnswer { get; set; }
+
+    public static string CalculateMd5Hash(string input)
+    {
+        var inputBytes = Encoding.ASCII.GetBytes(input);
+        var hash = MD5.HashData(inputBytes);
+        var sb = new StringBuilder();
+        foreach (var t in hash)
+        {
+            sb.Append(t.ToString("X2"));
+        }
+
+        return sb.ToString();
+    }
+
+    public static void Progress(long current, long total)
+    {
+        Progress((int)(current * 100 / total));
+    }
+
+    public static void Progress(int percent)
+    {
+        Console.CursorLeft = 0;
+        Console.Write("|");
+        for (var i = 0; i < percent; i++)
+        {
+            Console.Write("\u2588");
+        }
+        for (var i = percent; i < 100; i++)
+        {
+            Console.Write("_");
+        }
+        Console.Write($"| {percent}%");
+    }
+}
